5e822c1045571f83f1ec6d82cc2aafda274473e7
imren88 <imren@secer.com.tr>
2025-05-31 23:09:10 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hol1",
+      "name": "hol1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7234226833758521,
+          "pitch": 0.46933743467502254,
+          "rotation": 0,
+          "target": "1-hol2"
+        },
+        {
+          "yaw": -0.6302909051232586,
+          "pitch": 0.14629222577743306,
+          "rotation": 0,
+          "target": "5-ebeveyn-yatak-odas"
+        },
+        {
+          "yaw": 0.6052176667134876,
+          "pitch": 0.08256690936849864,
+          "rotation": 0,
+          "target": "7-ocuk-odas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hol2",
+      "name": "hol2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1233095212284887,
+          "pitch": 0.0498910088723008,
+          "rotation": 0,
+          "target": "2-salon"
+        },
+        {
+          "yaw": 0.42781180930845153,
+          "pitch": 0.06001405636262369,
+          "rotation": 0,
+          "target": "3-mutfak"
+        },
+        {
+          "yaw": 0.7052218785720292,
+          "pitch": 0.07373550485738534,
+          "rotation": 0,
+          "target": "4-banyo"
+        },
+        {
+          "yaw": -1.7770785314049675,
+          "pitch": 0.6530620814295478,
+          "rotation": 0,
+          "target": "0-hol1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.132161303424809,
+        "pitch": 0.5639270480301661,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8116759598379311,
+          "pitch": -0.06651446989518028,
+          "rotation": 0,
+          "target": "1-hol2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-mutfak",
+      "name": "mutfak",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.347670530846397,
+          "pitch": -0.010699307145836556,
+          "rotation": 0,
+          "target": "1-hol2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-banyo",
+      "name": "banyo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.752901419018153,
+          "pitch": -0.0771849707428025,
+          "rotation": 0,
+          "target": "1-hol2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ebeveyn-yatak-odas",
+      "name": "ebeveyn yatak odası",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.4504469486510043,
+        "pitch": -0.04685896632972586,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6866812693349669,
+          "pitch": 0.13775492616342078,
+          "rotation": 0,
+          "target": "0-hol1"
+        },
+        {
+          "yaw": -0.9618637402877788,
+          "pitch": 0.09967283977621832,
+          "rotation": 0,
+          "target": "6-ebeveyn-banyo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ebeveyn-banyo",
+      "name": "ebeveyn banyo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8104844795378163,
+          "pitch": 0.71522957475403,
+          "rotation": 0,
+          "target": "5-ebeveyn-yatak-odas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-ocuk-odas",
+      "name": "çocuk odası",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0539153004181934,
+          "pitch": 0.07730173644308458,
+          "rotation": 0,
+          "target": "0-hol1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "308/5",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
